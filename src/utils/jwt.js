141f97fbea2ba9d06ecac265e4c52df226b4aee8
Md Nazim Hossain <86850859+md-nazim-Hossain@users.noreply.github.com>
2024-07-06 17:47:05 +0600

    fix: :zap: fix the jwt mailformed error

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -2,7 +2,8 @@ import jwt from "jsonwebtoken";
 import { config } from "../config/index.js";
 
 export const getUserIdFromToken = (req) => {
-  if (!(req.cookies.accessToken || req.headers?.Authorization || req.headers.authorization)) return null;
+  const getToken = req.cookies?.accessToken || req.headers?.Authorization || req.headers.authorization;
+  if (!getToken || getToken.split(" ")[1] === "undefined") return null;
   const token =
     req.cookies?.accessToken || req.headers?.Authorization?.split(" ")[1] || req.headers.authorization.split(" ")[1];
   if (!token) {
