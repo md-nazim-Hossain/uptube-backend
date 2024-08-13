6d1fe1f0e27707fd80ebf8b441c9318b87935fc9
Md Nazim Hossain <86850859+md-nazim-Hossain@users.noreply.github.com>
2024-08-13 19:29:54 +0600

    fix: :fire: fix the getUserLikeVideo api

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -6,7 +6,13 @@ import StatusCode from "http-status-codes";
 import { paginationHelpers } from "../utils/paginationHelpers.js";
 
 const getUserLikeVideos = catchAsync(async (req, res) => {
-  const likeVideoCount = await Like.countDocuments({ likedBy: req.user._id, video: { $ne: null } });
+  const likeVideoCount = await Like.countDocuments({
+    likedBy: new mongoose.Types.ObjectId(req.user._id),
+    video: { $ne: null },
+  });
+  if (!likeVideoCount)
+    return sendApiResponse({ res, statusCode: StatusCode.OK, data: [], message: "No like videos found" });
+
   const { meta, skip, limit } = paginationHelpers(req, likeVideoCount);
   const videoMatcher = [
     { $match: { isPublished: true } },
@@ -23,16 +29,14 @@ const getUserLikeVideos = catchAsync(async (req, res) => {
     { $addFields: { owner: { $arrayElemAt: ["$owner", 0] } } },
   ];
   const type = req.query.type;
-  if (type) {
+  if (type && type === "video") {
     videoMatcher.push({ $match: { type } });
   }
 
   const likeVideos = await Like.aggregate([
-    { $match: { likedBy: new mongoose.Types.ObjectId(req.user._id) } },
-    { $match: { video: { $ne: null } } },
+    { $match: { likedBy: new mongoose.Types.ObjectId(req.user._id), video: { $ne: null } } },
+
     { $sort: { createdAt: -1 } },
-    { $skip: skip },
-    { $limit: limit },
     {
       $lookup: {
         from: "videos",
@@ -44,6 +48,8 @@ const getUserLikeVideos = catchAsync(async (req, res) => {
     },
     { $addFields: { video: { $arrayElemAt: ["$video", 0] } } },
     { $unwind: "$video" },
+    { $limit: limit },
+    { $skip: skip },
   ]);
 
   return sendApiResponse({
