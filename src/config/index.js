6a7a3ac03b1fbdf77e13169fc28b66d9ccbb51cc
Md Nazim Hossain <86850859+md-nazim-Hossain@users.noreply.github.com>
2024-08-20 20:58:50 +0600

    refactor: :fire: remove resend api key

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,6 @@
 export const config = {
   port: process.env.PORT || 5000,
   db_url: process.env.DATABASE_URL,
-  resend_api_key: process.env.RESEND_API_KEY,
   from_email: process.env.FROM_EMAIL,
   clientUrl: process.env.NODE_ENV === "production" ? "https://uptube.vercel.app" : "http://localhost:3000",
   constants: {
--- src/utils/send-email.js
+++ src/utils/send-email.js
@@ -1,17 +1,6 @@
 import nodemailer from "nodemailer";
 import { config } from "../config/index.js";
-// export const sendEmail = async (email, subject, text) => {
-//   const resendEmail = new Resend(config.resend_api_key);
-//   return await resendEmail.emails.send({
-//     from: config.from_email,
-//     to: email,
-//     subject: subject,
-//     html: `<section>
-//           <h1>${subject}</h1>
-//           ${text}
-//       </section>`,
-//   });
-// };
+
 const transporter = nodemailer.createTransport({
   service: "gmail",
   host: "smtp.gmail.email",
