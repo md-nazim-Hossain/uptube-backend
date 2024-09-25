36550939b9e83710d5de013144a8fca5fa29cb65
Md Nazim Hossain <86850859+md-nazim-Hossain@users.noreply.github.com>
2024-09-25 20:30:14 +0600

    remove redis client

--- src/utils/catchAsync.js
+++ src/utils/catchAsync.js
@@ -2,7 +2,7 @@ import { redisClient } from "../db/redisClient.js";
 
 const catchAsync = (fn) => async (req, res, next) => {
   try {
-    if (!redisClient.isOpen) await redisClient.connect();
+    // if (!redisClient.isOpen) await redisClient.connect();
     return await fn(req, res, next);
   } catch (error) {
     next(error);
