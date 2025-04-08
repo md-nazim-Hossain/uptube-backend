c18f42a2d29dc45761679ca88604b5f4c7c77849
Md Nazim Hossain <86850859+md-nazim-Hossain@users.noreply.github.com>
2025-04-08 22:16:30 +0600

    fix: :fire: update db url in db.js file

--- src/db/db.js
+++ src/db/db.js
@@ -1,10 +1,9 @@
 import mongoose from "mongoose";
-import { config } from "../config/index.js";
 import logger from "../utils/logger.js";
 
 async function connectDB() {
   try {
-    const connectionInstance = await mongoose.connect(`${config.db_url}/${config.constants.db_name}`);
+    const connectionInstance = await mongoose.connect(process.env.DATABASE_URL);
     logger.info(`Connected to database !! DB_HOST:${connectionInstance.connection.host}`);
   } catch (error) {
     logger.error("MongoDb Connection Failed:", error);
